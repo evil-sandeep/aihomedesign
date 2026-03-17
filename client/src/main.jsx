7103a411f8b5d613a8b7b1eea56b8a4df2a53e0f
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2026-03-17 23:55:08 +0530

    Integrate Vercel Analytics and update task list

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
+      <Analytics />
     </GoogleOAuthProvider>
   </StrictMode>,
 )
